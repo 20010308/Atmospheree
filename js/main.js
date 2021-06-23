d9d98c844d21237b20410aa3657fe3854b21e22a
aftuit <farruxaktamov6@gmail.com>
2021-06-23 18:22:19 +0500

    pushed to card

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
+$(document).ready(function(){
+    
+$('a').on('click', function(e){
+    e.preventDefault();
+    var pageRef = $(this).attr('href');
+
+    callPage(pageRef)
+});
+
+
+function callPage(pageRefInput){
+     $.ajax({
+        url: pageRefInput,
+
+        type: "GET",
+
+        dataType: 'text',
+
+        success: function(response){
+            console.log('the page was loaded', reponse)
+            $('.main__test').innerHtml(response);
+        },
+
+        error: function(error){
+            console.log('the page was Not loaded', error);
+        },
+
+        complete: function(xhr, status){
+            console.log('completed');
+        }
+
+    });
+}
+
+
+
+
+
 AOS.init();
+
 
 // Clock
 var date = new Date();
@@ -13,6 +52,7 @@ document.getElementById('minut').innerHTML = min;
 
 
 // Задачи на день
+
 let todolist = [];
 
 function drawpage() {
@@ -47,7 +87,8 @@ function drawByTime() {
     let starting = document.getElementById('start-time');
     let ending = document.getElementById('end-time');
     for (let i = 0; i < byTimeToDo.length; i++) {
-        result1 += "<div class='tasks'> <div> <span>" + starting.value + ' ' + ending.value + ' ' + byTimeToDo[i] + "</span></div> <div class='task-close' onclick='deleteByTimeTask(" + i + ")'>&times;</div></div>"
+        result1 += "<div class='tasks'> <div> <span>" + starting.value + ' ' +
+             ending.value + ' ' + byTimeToDo[i] + "</span></div> <div class='task-close' onclick='deleteByTimeTask(" + i + ")'>&times;</div></div>"
     }
 
     document.getElementById("todolist-date").innerHTML = result1;
@@ -69,9 +110,18 @@ function deleteByTimeTask(index) {
     byTimeToDo.splice(index, 1);
     drawByTime();
 }
+// ====================================================
+// ====================================================
+
+
+let workList = {
+    card_area: "",
+    card_area_list: ""
+}
 
 
 let workarea_card = [];
+
 function workListFunction() {
     let newCard = {
         cardList: []
@@ -80,35 +130,37 @@ function workListFunction() {
     workarea_card.push(newCard);
     addWorkArea();
 }
-function addWorkArea() {
-    let card_area = "";
+
+//  workarea_card[0] = 
+
+function ContactWithClient() {
+    workList.card_area = "";
     for (let i = 0; i < workarea_card.length; i++) {
-        let card_area_list = "";
+        workList.card_area_list = "";
         for (let j = 0; j < workarea_card[i].cardList.length; j++) {
-            card_area_list +=
-                "<div class='contact__member mt-3'>" +
+            workList.card_area_list +=
+                "<div class='contact__member mt-2'>" +
                 "<span class='fas fa-user-circle text-secondary'></span>" +
                 "<i class='liner'></i>" +
-                "<input type='text' id='contact-" + i + "' placeholder='Имя контакта, телефон или e-mail'>" +
-                "<span class='fas fa-search text-secondary'></span>" +
-                "</div>"
+                "<input type='text' id='contact-" + i + j + "' placeholder='Имя контакта, телефон или e-mail'>" +
+                "<span class='fas fa-search text-secondary' onclick='consoleid(" + i + j + ")'></span>" +
+                "</div>";
         }
-        card_area +=
+        workList.card_area +=
             "<div class='workarea__card' id='workarea-card-" + i + "'>" +
             "<div class='workarea__card__header'>" +
             "<span class='workarea__title'>Название</span>" +
             "<span class='title__count font-weight-normal'>0</span>" +
-            "<i class='top__transparent blue'></i>" +
-            "<div class='plus_area font-weight-bold'>+</div>" +
+            "<div class='plus_area font-weight-bold' onclick='workListFunction()'>+</div>" +
             "</div>" +
             "<p class='font-opensans-regular'><span>0</span> руб.</p>" +
-            "<button class='main' data-toggle='collapse'  onclick='consolelog(" + i + ")'><i class='fas fa-plus'></i></button>" +
-            "<form method='GET' class='card__form '>" +
+            "<button class=' accordion' data-toggle='collapse'  onclick='consolelog(" + i + ")'><i class='fas fa-plus'></i></button>" +
+            "<form method='GET' class='card__form panel'>" +
 
             "<span class='font-opensans-semibold'>Клиент</span>" +
             "<div class='card__form__body'>" +
-            "<label for='contact' class='font-opensans-semibold'>Контакт</label>" +       
-            "<div>" + card_area_list + "</div>"+
+            "<label for='contact' class='font-opensans-semibold'>Контакт</label>" +
+            "<div>" + workList.card_area_list + "</div>" +
             "<p class='font-opensans-semibold' onclick='addContact(" + i + ")'><i class='fas fa-plus'></i> Добавить участника</p>" +
             "<label for='company' class='font-opensans-semibold'>Компания</label>" +
             "<div class='contact__member'>" +
@@ -139,9 +191,77 @@ function addWorkArea() {
             "<button>Отменить</button>" +
             "</div>" +
             "</form>" +
-            "</div>"
+            "</div>";
     }
-    document.getElementById('main-workarea').innerHTML = card_area;
+}
+
+
+function ContactArea(i, j) {
+    "<div class='contact__member '>" +
+        "<span class='fas fa-user-circle text-secondary'></span>" +
+        "<i class='liner'></i>" +
+        "<input type='text' id='contact-" + i + j + "' placeholder='Имя контакта, телефон или e-mail'>" +
+        "<span class='fas fa-search text-secondary' onclick='consoleid(" + i + j + ")'></span>" +
+        "</div>"
+}
+
+function Clients(i) {
+    "<div class='workarea__card' id='workarea-card-" + i + "'>" +
+        "<div class='workarea__card__header'>" +
+        "<span class='workarea__title'>Название</span>" +
+        "<span class='title__count font-weight-normal'>0</span>" +
+        "<div class='plus_area font-weight-bold' onclick='workListFunction()'>+</div>" +
+        "</div>" +
+        "<p class='font-opensans-regular'><span>0</span> руб.</p>" +
+
+        "<button class='main accordion'  onclick='consolelog(" + i + ")'><i class='fas fa-plus'></i></button>" +
+
+        "<form method='GET' class='card__form panel'>" +
+
+        "<span class='font-opensans-semibold'>Клиент</span>" +
+
+        "<div class='card__form__body'>" +
+        "<label for='contact' class='font-opensans-semibold'>Контакт</label>" +
+        "<div>" + workList.card_area_list + "</div>" +
+        "<p class='font-opensans-semibold' onclick='addContact(" + i + ")'><i class='fas fa-plus'></i> Добавить участника</p>" +
+        "<label for='company' class='font-opensans-semibold'>Компания</label>" +
+        "<div class='contact__member'>" +
+        "<span class='fas fa-building text-secondary'></span>" +
+        "<i class='liner'></i>" +
+        "<input type='text' id='company' placeholder='Имя контакта, телефон или e-mail'>" +
+        "<span class='fas fa-search text-secondary'></span>" +
+        "</div>" +
+        "</div>" +
+        "<div class='card__form__footer'>" +
+        "<label for='name' class='font-opensans-semibold'>Название</label>" +
+        "<input type='text' id='name' placeholder='Сделка #'>" +
+        "<label for='timer' class='font-opensans-semibold'>Сумма и валюта</label>" +
+        "<div class='d-flex time__bg'>" +
+        "<input type='text' id='timer' value='0.00'>" +
+        "<select>" +
+        "<option value='1'>Российский рубль</option>" +
+        "<option value='2'>Доллар США</option>" +
+        "<option value='3'>Евро</option>" +
+        "<option value='4'>Гривна</option>" +
+        "<option value='5'>Белорусский рубль</option>" +
+        "</select>" +
+        "</div>" +
+        "<span class='select__pole font-opensans-semibold'>Выбрать поле</span>" +
+        "</div>" +
+        "<div class='submit__card d-flex'>" +
+        "<button>Сохранить</button>" +
+        "<button>Отменить</button>" +
+        "</div>" +
+        "</form>" +
+        "</div>"
+
+      
+}
+
+function addWorkArea() {
+    console.log("added")
+    ContactWithClient();
+    document.getElementById('main-workarea').innerHTML = workList.card_area;
 }
 
 function addContact(index) {
@@ -150,4 +270,25 @@ function addContact(index) {
     addWorkArea();
 }
 
+function consoleid(id) {
+    console.log(id);
+}
 
+// ===============================  
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.padding = '0';
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.padding = "13px";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+});
